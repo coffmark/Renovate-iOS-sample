3bcdebd6f20a1ee1b71b194cdd5a68dfe527c2a8
coffmark <come1234560@gmail.com>
2022-08-01 14:09:15 +0900

    initial commit

---
+++ Renovate-iOS-sample/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Renovate-iOS-sample
+//
+//  Created by 神村　亮佑　 on 2022/08/01.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
