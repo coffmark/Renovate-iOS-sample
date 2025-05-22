ae2b87be8b24b834f8f39e32aeec1bf9c3fed2e2
coffmark <come1234560@gmail.com>
2025-05-22 16:24:46 +0900

    firebaseを追加

--- Pacakges/PackageA/Package.swift
+++ Pacakges/PackageA/Package.swift
@@ -12,7 +12,7 @@ let package = Package(
             targets: ["PackageA"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/Alamofire/Alamofire.git", exact: Version("5.9.0")),
+        .package(url: "https://github.com/firebase/firebase-ios-sdk.git", exact: Version("11.5.0"))
     ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
